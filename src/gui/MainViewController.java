78fafcffb20845e7debbea80b1b5feee101c1240
Danielle Soares <silvasoaresdanielle2@gmail.com>
2024-10-13 14:51:55 -0300

    main view controller

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,38 @@
+package gui;
+
+import java.awt.MenuItem;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class MainViewController implements Initializable{
+	
+	@FXML
+	private MenuItem menuItemSeller;
+	@FXML
+	private MenuItem menuItemDepartment;
+	@FXML
+	private MenuItem menuItemAbout;
+	
+	@FXML
+	public void onMenuItemSellerAction() {
+
+	}
+	
+	@FXML
+	public void onMenuItemDepartmentAction() {
+
+	}
+	
+	@FXML
+	public void onMenuItemAboutAction() {
+
+	}
+
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {
+		
+	}
+}
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module workshopJavaFX {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires java.desktop;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
